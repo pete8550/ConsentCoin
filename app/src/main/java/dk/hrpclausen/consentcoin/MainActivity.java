68d7e440ca9a3187178910eab8d79bffb4106606
pete8550 <42993922+pete8550@users.noreply.github.com>
2019-05-15 20:00:53 +0200

    Small edits in drawer and MainActivity

--- app/src/main/java/dk/hrpclausen/consentcoin/MainActivity.java
+++ app/src/main/java/dk/hrpclausen/consentcoin/MainActivity.java
@@ -58,8 +58,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_tools) {
 
-        } else if (id == R.id.nav_share) {
-
         } else if (id == R.id.nav_send) {
 
         }
