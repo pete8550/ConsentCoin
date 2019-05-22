c532944c067d511c2652b889bf14c4f801a5bd4b
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2019-05-22 15:40:23 +0200

    Person-klasse oprettet

---
+++ app/src/main/java/dk/hrpclausen/consentcoin/Person.java
@@ -0,0 +1,52 @@
+package dk.hrpclausen.consentcoin;
+
+public class Person {
+
+    String name;
+    String Email;
+    int Number;
+
+    public Person(String name, String email, int number) {
+        this.name = name;
+        Email = email;
+        Number = number;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, String email) {
+        this.name = name;
+        Email = email;
+    }
+
+    public Person(String name, int number) {
+        this.name = name;
+        Number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public void setNumber(int number) {
+        Number = number;
+    }
+}
