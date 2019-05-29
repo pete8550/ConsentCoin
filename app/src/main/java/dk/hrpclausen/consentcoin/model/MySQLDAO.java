29f542013c6bffa7e35e51cc2073981bf70a6a84
EmilDino <EmilLD123@hotmail.com>
2019-05-29 11:07:04 +0200

    har tilføjet et DAO interfase

---
+++ app/src/main/java/dk/hrpclausen/consentcoin/model/MySQLDAO.java
@@ -0,0 +1,13 @@
+package dk.hrpclausen.consentcoin.model;
+// Er ikke implementeret
+public class MySQLDAO implements DatabaseDAO {
+    @Override
+    public void pushToDatabaseConsent(String text) {
+
+    }
+
+    @Override
+    public void pushToDatabaseAnmodning(String text) {
+
+    }
+}
