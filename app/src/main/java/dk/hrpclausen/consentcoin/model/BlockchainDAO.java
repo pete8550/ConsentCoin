25fe69ff46ab1b976cae5778e94915bd36b9d2ab
EmilDino <EmilLD123@hotmail.com>
2019-05-29 11:23:19 +0200

    har tilføjet en blockchain DAO

---
+++ app/src/main/java/dk/hrpclausen/consentcoin/model/BlockchainDAO.java
@@ -0,0 +1,13 @@
+package dk.hrpclausen.consentcoin.model;
+
+public class BlockchainDAO implements DatabaseDAO{
+    @Override
+    public void pushToDatabaseConsent(String text) {
+
+    }
+
+    @Override
+    public void pushToDatabaseAnmodning(String text) {
+
+    }
+}
