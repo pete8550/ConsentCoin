4264a21b0a713baebc067df2397a987de762dd6b
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2019-05-31 11:42:06 +0200

    Klassediagramer

--- app/src/main/java/dk/hrpclausen/consentcoin/model/FirebaseDAO.java
+++ app/src/main/java/dk/hrpclausen/consentcoin/model/FirebaseDAO.java
@@ -17,4 +17,5 @@ public class FirebaseDAO implements DatabaseDAO {
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Anmodninger");
         databaseReference.push().setValue(text);
     }
+
 }
